30043791856154f988eefac2a618bd0fa3e0a4b7
Karim Zeidan <karemziedan@gmail.com>
2024-05-24 21:29:04 +0300

    nothing

--- cheatsheet.py
+++ cheatsheet.py
@@ -1,2 +1,3 @@
 isinstance(cheatsheet, str)#will return True if the object is of the specified type, otherwise False    
 words=TEXTS.split()#will return a list of words in the text     
+#this should be updated
